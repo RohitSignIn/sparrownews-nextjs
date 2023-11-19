55dfbd02ef82017689263c9063ac1ff6693d8c19
RohitSignIn <rohitsinghsignin@gmail.com>
2023-11-19 14:52:46 +0530

    Added Blog route | api to fetch, update and delete blog

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,71 @@
+import Blogs from "@/models/blogModel";
+import qs from "qs";
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/config/dbConfig";
+
+connect();
+
+// Fetch Blog
+export async function GET(req: NextRequest) {
+  try {
+    const rawSlug = req.url.split("?")[1];
+    const slug = qs.parse(rawSlug).slug;
+
+    const Blog = await Blogs.findOne({ slug: slug });
+
+    return NextResponse.json({
+      success: true,
+      data: Blog,
+      status: 200,
+    });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
+
+// Update BLOG
+export async function PUT(req: NextRequest) {
+  const reqBody = await req.json();
+
+  try {
+    if (reqBody._id) {
+      const updatedBlog = await Blogs.findByIdAndUpdate(
+        reqBody._id,
+        reqBody.update,
+        { new: true }
+      );
+
+      return NextResponse.json({
+        success: true,
+        data: updatedBlog,
+        status: 201,
+      });
+    }
+
+    return NextResponse.json({
+      success: false,
+      message: "Missing Fields, _id not found",
+      status: 400,
+    });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
+
+// Delete Blog
+export async function DELETE(req: NextRequest) {
+  try {
+    const reqBody = await req.json();
+    const { _id } = reqBody;
+
+    const deletedBlog = await Blogs.findByIdAndDelete({ _id: _id });
+
+    return NextResponse.json({
+      success: true,
+      data: deletedBlog,
+      status: 201,
+    });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
