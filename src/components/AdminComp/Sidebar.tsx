c86fd7cf63bb3f12252e4db2cc06ca235fdb2457
RohitSignIn <rohitsinghsignin@gmail.com>
2023-12-06 11:25:01 +0530

    Remove / from href in Sidebar

--- src/components/AdminComp/Sidebar.tsx
+++ src/components/AdminComp/Sidebar.tsx
@@ -6,25 +6,25 @@ function Sidebar() {
     <div id='sideNav' className='w-[250px] p-2 bg-primary h-full sticky top-0'>
       <Link
         className='block rounded-md hover:bg-slate-200 p-2 hover:text-slate-800'
-        href='admin/add-post'
+        href='/admin/add-post'
       >
         Add Post
       </Link>
       <Link
         className='block rounded-md hover:bg-slate-200 p-2 hover:text-slate-800'
-        href='admin/posts'
+        href='/admin/posts'
       >
         Posts
       </Link>
       <Link
         className='block rounded-md hover:bg-slate-200 p-2 hover:text-slate-800'
-        href='admin/logout'
+        href='/admin/logout'
       >
         Logout
       </Link>
       <Link
         className='block rounded-md hover:bg-slate-200 p-2 hover:text-slate-800'
-        href='admin/settings'
+        href='/admin/settings'
       >
         Settings
       </Link>
